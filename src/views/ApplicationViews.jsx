6e26403493f93ac2dd71e2f054c065e04fcfb983
Tyler Martin <tizzlerrm713@gmail.com>
2025-05-23 10:36:15 -0500

    initial commit

---
+++ src/views/ApplicationViews.jsx
@@ -0,0 +1,31 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import { NavBar } from "../components/nav/NavBar.jsx";
+import { Welcome } from "../components/welcome/Welcome.jsx";
+import { useEffect, useState } from "react";
+
+export const ApplicationViews = () => {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const locall2codeUser = localStorage.getItem("l2code_user");
+    const l2codeUserObject = JSON.parse(locall2codeUser);
+
+    setCurrentUser(l2codeUserObject);
+  }, []);
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<Welcome currentUser={currentUser} />} />
+      </Route>
+    </Routes>
+  );
+};
